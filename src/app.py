f6b41f67f80ccbf72a2598d598db6a884028fb91
Abdoulaye Diallo <abdoulayediallo.dallein@outlook.com>
2023-11-22 15:41:37 +0100

    first commit

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, send_file
 from PIL import Image
-from flask_mail import Mail, Message
+# from flask_mail import Mail, Message
 
 
 
@@ -21,13 +21,13 @@ def convert():
         # Envoi du PDF en téléchargement
         return send_file(pdf_path, as_attachment=True)
 
-@app.route('/send_email', methods=['POST'])
-def send_email():
-    msg = Message('Sujet de l\'e-mail', sender='ablodiallo1521@gmail.com', recipients=['ablodiallo1521@gmail.com'])
-    msg.body = 'Corps de l\'e-mail'
-    with app.open_resource('output.pdf') as pdf_attachment:
-        msg.attach('output.pdf', 'application/pdf', pdf_attachment.read())
-    mail.send(msg)  
-mail = Mail(app)
+# @app.route('/send_email', methods=['POST'])
+# def send_email():
+#     msg = Message('Sujet de l\'e-mail', sender='ablodiallo1521@gmail.com', recipients=['ablodiallo1521@gmail.com'])
+#     msg.body = 'Corps de l\'e-mail'
+#     with app.open_resource('output.pdf') as pdf_attachment:
+#         msg.attach('output.pdf', 'application/pdf', pdf_attachment.read())
+#     mail.send(msg)  
+# mail = Mail(app)
 if __name__ == '__main__':
     app.run(debug=True)
